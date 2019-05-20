36430a8b6a949c7fd15dc6db759db07490758891
Jaroslav Týc <mail@jaroslavtyc.com>
2019-05-20 17:45:57 +0200

    Fixed domains used for projects

--- _docker/docker.sh
+++ _docker/docker.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-nohup sh -c php-fpm &
+nohup php-fpm &
 
-cd /var/www/blog/ && nohup sh -c node_modules/.bin/gulp &
+cd /var/www/blog/ && nohup sudo -u www-data sh -c node_modules/.bin/gulp &
 
 /usr/local/bin/caddy -conf /etc/caddy/Caddyfile
